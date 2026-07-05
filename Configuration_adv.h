e79c1b229f12a7251dc0947d7fdb7ea0c693560d
jimmihenry <89158909+jimmihenry@users.noreply.github.com>
2026-07-05 21:20:12 +0200

    Update Configuration_adv.h
    HYBRID_THRESHOLD to 0 @ X,Y,Z, E0.
    Back to stealth chop2 for M569 S1 or S0 or back to Hybrid by putting in:
    
    #define HYBRID_THRESHOLD
    
      #define X_HYBRID_THRESHOLD     100  // [mm/s]
      #define X2_HYBRID_THRESHOLD    100
      #define Y_HYBRID_THRESHOLD     100
      #define Y2_HYBRID_THRESHOLD    100
      #define Z_HYBRID_THRESHOLD       3
      #define Z2_HYBRID_THRESHOLD      3
      #define Z3_HYBRID_THRESHOLD      3
      #define Z4_HYBRID_THRESHOLD      3
      #define I_HYBRID_THRESHOLD       3  // [linear=mm/s, rotational=°/s]
      #define J_HYBRID_THRESHOLD       3  // [linear=mm/s, rotational=°/s]
      #define K_HYBRID_THRESHOLD       3  // [linear=mm/s, rotational=°/s]
      #define U_HYBRID_THRESHOLD       3  // [mm/s]
      #define V_HYBRID_THRESHOLD       3
      #define W_HYBRID_THRESHOLD       3
      #define E0_HYBRID_THRESHOLD     30
      #define E1_HYBRID_THRESHOLD     30
      #define E2_HYBRID_THRESHOLD     30
      #define E3_HYBRID_THRESHOLD     30
      #define E4_HYBRID_THRESHOLD     30
      #define E5_HYBRID_THRESHOLD     30
      #define E6_HYBRID_THRESHOLD     30
      #define E7_HYBRID_THRESHOLD     30

--- Configuration_adv.h
+++ Configuration_adv.h
@@ -3048,7 +3048,7 @@
     #define STEALTHCHOP_U
     #define STEALTHCHOP_V
     #define STEALTHCHOP_W
-    //#define STEALTHCHOP_E
+    #define STEALTHCHOP_E
   #endif
 
   /**
@@ -3120,14 +3120,14 @@
    * STEALTHCHOP_(XY|Z|E) must be enabled to use HYBRID_THRESHOLD.
    * M913 X/Y/Z/E to live tune the setting
    */
-  //#define HYBRID_THRESHOLD
+  #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
-  #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     100
-  #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD       3
-  #define Z2_HYBRID_THRESHOLD      3
+  #define X_HYBRID_THRESHOLD       0  // [mm/s] was 100
+  #define X2_HYBRID_THRESHOLD    100  
+  #define Y_HYBRID_THRESHOLD       0  // was 100
+  #define Y2_HYBRID_THRESHOLD      0
+  #define Z_HYBRID_THRESHOLD       3  // was 3
+  #define Z2_HYBRID_THRESHOLD      3  // was 3
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
   #define I_HYBRID_THRESHOLD       3  // [linear=mm/s, rotational=°/s]
@@ -3136,7 +3136,7 @@
   #define U_HYBRID_THRESHOLD       3  // [mm/s]
   #define V_HYBRID_THRESHOLD       3
   #define W_HYBRID_THRESHOLD       3
-  #define E0_HYBRID_THRESHOLD     30
+  #define E0_HYBRID_THRESHOLD      0  // was 30
   #define E1_HYBRID_THRESHOLD     30
   #define E2_HYBRID_THRESHOLD     30
   #define E3_HYBRID_THRESHOLD     30
